529cba36eba4eec2455c65f4f533140f0ce321ae
SOURAV MANDAL <sourav0200@gmail.com>
2022-11-11 21:47:34 +0600

    Bird class added

---
+++ Animal.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Animal{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
