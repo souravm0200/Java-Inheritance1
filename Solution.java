be2f27c23979b859f09011916ad26312ab8495b5
SOURAV MANDAL <sourav0200@gmail.com>
2022-11-11 21:50:29 +0600

    all file added

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+
+   public static void main(String args[]){
+
+	  Bird bird = new Bird();
+	  bird.walk();
+	  bird.fly();
+      bird.sing();
+	
+   }
+}
